4e9edc7000047750d3c01a6ebec760e2536a5ebc
mvaneijgen <mvaneijgen@gmail.com>
2023-03-06 09:11:30 +0100

    Copy from split numpad

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,89 @@
+/* Copyright 2019 Mitchel van Eijgen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+<<<<<<< HEAD
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT( /* Base */
+    KC_DEL, KC_MPLY, KC_PEQL, KC_PSLS, KC_PAST, 
+    KC_PGUP, KC_7, KC_8, KC_9, KC_PMNS, 
+    KC_PGDN, KC_4, KC_5, KC_6, KC_PPLS, 
+    KC_MUTE, KC_1, KC_2, KC_3, KC_ENT, 
+    RESET, KC_0, KC_DOT 
+  ),
+  [1] = LAYOUT( 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______ 
+  ),
+  [2] = LAYOUT( 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______ 
+  ),  
+  [3] = LAYOUT( 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, 
+    _______, _______, _______ 
+  ),
+};
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
+=======
+#define _BASE 0
+#define _GAME 1
+#define _RESET 2
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT( /* Base */
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_MUTE, 
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_MPLY, 
+		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC__VOLUP, 
+		KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_UP, KC__VOLDOWN, 
+		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_GAME), MO(_RESET), KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+  [_GAME] = LAYOUT( /* Game */
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_MUTE, 
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_MPLY, 
+		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC__VOLUP, 
+		KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_BRMU, KC__VOLDOWN, 
+		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_NO, KC_NO, KC_MEDIA_REWIND, KC_BRMD, KC_MEDIA_FAST_FORWARD
+  ),
+  [_RESET] = LAYOUT( /* Game */
+		RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+};
+>>>>>>> 04c24e94f83adb00b2b44784edc5f3408e4c5e0d
