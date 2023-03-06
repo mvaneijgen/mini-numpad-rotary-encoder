ed97748d67a1c26a588499ccdb3044845ceb4494
mvaneijgen <mvaneijgen@gmail.com>
2023-03-06 11:26:17 +0100

    Default build nog working

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -14,76 +14,39 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-<<<<<<< HEAD
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-    KC_DEL, KC_MPLY, KC_PEQL, KC_PSLS, KC_PAST, 
-    KC_PGUP, KC_7, KC_8, KC_9, KC_PMNS, 
-    KC_PGDN, KC_4, KC_5, KC_6, KC_PPLS, 
-    KC_MUTE, KC_1, KC_2, KC_3, KC_ENT, 
-    RESET, KC_0, KC_DOT 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS
   ),
   [1] = LAYOUT( 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______ 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS
   ),
   [2] = LAYOUT( 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______ 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS
   ),  
   [3] = LAYOUT( 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, 
-    _______, _______, _______ 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS
   ),
 };
 
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
-}
-=======
-#define _BASE 0
-#define _GAME 1
-#define _RESET 2
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT( /* Base */
-		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_MUTE, 
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_MPLY, 
-		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC__VOLUP, 
-		KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_UP, KC__VOLDOWN, 
-		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(_GAME), MO(_RESET), KC_LEFT, KC_DOWN, KC_RGHT
-  ),
-  [_GAME] = LAYOUT( /* Game */
-		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_MUTE, 
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_MPLY, 
-		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC__VOLUP, 
-		KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_BRMU, KC__VOLDOWN, 
-		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_NO, KC_NO, KC_MEDIA_REWIND, KC_BRMD, KC_MEDIA_FAST_FORWARD
-  ),
-  [_RESET] = LAYOUT( /* Game */
-		RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [1] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [2] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
+    [3] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
 };
->>>>>>> 04c24e94f83adb00b2b44784edc5f3408e4c5e0d
+#endif
